048c646cf34f3d243acffd78681526fdd7e5eafc
kjelloh <kjell-olov.hogdahl@itfied.se>
2026-09-21 14:41:21 +0200

    Introduced HTML2MarkdownParser::print_to_log and called it on html parser events

--- session/71388c86/eml_to_markdown.py
+++ session/71388c86/eml_to_markdown.py
@@ -188,12 +188,16 @@ class HTML2MarkdownParser(HTMLParser):
         self.email_ast(),
         self.markdown
 
+    def print_to_log(self,entry: str) -> None:
+        print(f"print_to_log:'{entry}'")
+        self.log.append(entry)
+
     # -------------------------------------------------------------------
     # Markdown parser - BEGIN
     # -------------------------------------------------------------------
 
     def to_markdown_apply_void_html(self,tag,attrs_dict: dict) -> None:
-        print(f"{'.'.join(self.current_html_path)}: to_markdown_apply_void_html: attrs_dict:{attrs_dict}")
+        self.print_to_log(f"{'.'.join(self.current_html_path)}: to_markdown_apply_void_html: attrs_dict:{attrs_dict}")
         if self.current_attr:
             raise IncompleteParseError(
                 f"{'.'.join(self.current_html_path)}: Expected empty current attrs on void html attrs:{attrs_dict}"
@@ -204,7 +208,7 @@ class HTML2MarkdownParser(HTMLParser):
         return
 
     def to_markdown_apply_open_html(self,attrs_dict: dict) -> None:
-        print(f"{'.'.join(self.current_html_path)}: to_markdown_apply_open_html: attrs_dict:{attrs_dict}")
+        self.print_to_log(f"{'.'.join(self.current_html_path)}: to_markdown_apply_open_html: attrs_dict:{attrs_dict}")
         if self.current_attr:
             raise IncompleteParseError(
                 f"{'.'.join(self.current_html_path)}: Expected empty current attrs on open html attrs:{attrs_dict}"
@@ -214,11 +218,12 @@ class HTML2MarkdownParser(HTMLParser):
         return
 
     def to_markdown_apply_data(self,data: str) -> None:
-        print(f"{'.'.join(self.current_html_path)}: to_markdown_apply_data: data:{len(data)} chars")
+        self.print_to_log(f"{'.'.join(self.current_html_path)}: to_markdown_apply_data: data:{len(data)} chars")
         if self.current_data != "":
             raise IncompleteParseError(
                 f"{'.'.join(self.current_html_path)}: Expected empty (consumed) current data on open new html data"
-                f" unconsumed ==> '{data}'"
+                f"\n\tunconsumed ==> '{self.current_data}'"
+                f"\n\tdata:{data}"
                 f"\n<Parse LOG>\n{'\n'.join(self.log)}"
             )
 
@@ -228,7 +233,13 @@ class HTML2MarkdownParser(HTMLParser):
         return
 
     def to_markdown_apply_close_html(self) -> None:
-        print(f"{'.'.join(self.current_html_path)}: to_markdown_apply_close_html")
+        self.print_to_log(f"{'.'.join(self.current_html_path)}: to_markdown_apply_close_html")
+        if self.current_data != "":
+            raise IncompleteParseError(
+                f"{'.'.join(self.current_html_path)}: Expected empty (consumed) current data on open new html data"
+                f"\n\tunconsumed ==> '{self.current_data}'"
+                f"\n<Parse LOG>\n{'\n'.join(self.log)}"
+            )
         return
 
     # -------------------------------------------------------------------
